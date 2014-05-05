622104bb9deff6fe07819c13d1e927e6ce9da4c3
Ray Mangum <raymangum@gmail.com>
2014-05-05 19:00:57 -0400

    Task C4 complete

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -13,9 +13,9 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product price must be positive" do
-    product = Product.new(title:         "My Book Title",
-                          descritiption: "yyy",
-                          image_url:     "zzz.jpg")
+    product = Product.new(title:       "My Book Title",
+                          description: "yyy",
+                          image_url:   "zzz.jpg")
     product.price = -1
     assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
